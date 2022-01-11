85363a58d52614d07e60fa8887096bb78abbd67a
kimmedev <80742971+kimmekimme@users.noreply.github.com>
2022-01-11 16:22:10 +0900

    API, MVC, Static

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -13,11 +14,40 @@ public class HelloController {
         model.addAttribute("data", "kimme!!");
         return "hello"; // templates/hello.html (thymeleaf 템플릿 엔진 처리)
     }
+
     @GetMapping("hello-mvc")
-    public String helloMvc(@RequestParam(value = "name", required = true) String name, Model model){
+    public String helloMvc(@RequestParam(value = "name", required = true) String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template"; // templates/hello.html (thymeleaf 템플릿 엔진 처리)
-
     }
 
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+
+        return "hello " + name; // "hello spring"
+    }
+    //alt + insert: 디렉터리, 패키지, 클래스등 생성목록 단축키
+    //ctrl + space: 기본코드 자동완성
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloapi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; //ctrl + shift + enter 자동완성
+    }
+
+    static class Hello {
+        private String name;
+
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 }
+
