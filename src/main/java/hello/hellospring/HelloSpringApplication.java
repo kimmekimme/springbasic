7f6cfd4dd04bffec1cf21df8a1f4f2cbe4ab4279
kimmedev <80742971+kimmekimme@users.noreply.github.com>
2022-01-13 16:49:42 +0900

    TestCase

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -12,4 +12,5 @@ public class HelloSpringApplication {
 	}
 
 }
+//ctrl(command) + e: 최근파일목록
 
--- src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -1,32 +1,26 @@
 package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
-import org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-//shift + f10 이전 실행 재실행
-class MemberServiceTest {
-//ctrl + alt + 방향키 뷰전환
-    MemberService memberService;
-    MemoryMemberRepository memberRepository;
-    @BeforeEach
-    public void beforeEach(){
-        memberRepository = new MemoryMemberRepository();
-        memberService = new MemberService(memberRepository);
-    }
-    @AfterEach
-    public void afterEach(){
-        memberRepository.clearStore();
-    }
 
+@SpringBootTest //스프링 통합 테스트
+@Transactional //shift + f10 이전 실행 재실행
+class MemberServiceIntegrationTest {
+//ctrl + alt + 방향키 뷰전환
+    @Autowired MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository; //테스트니까 오토와이월드
 
     @Test
     public void 회원가입() throws Exception { //테스트는 한글 가능
@@ -39,8 +33,9 @@ class MemberServiceTest {
 
     //then 결과
         Member findMember = memberService.findOne(saveId).get();
-        assertThat(member.getName()).isEqualTo(findMember.getName());
+        assertEquals(member.getName(), findMember.getName());
     }
+
     @Test
     public void 중복회원예외() throws Exception{
         //given
@@ -53,6 +48,7 @@ class MemberServiceTest {
         //when
         memberService.join(member1);
         IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
         assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
 
         //then
